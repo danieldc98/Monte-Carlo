0d4c127f5c2b5256c50527abb1771a3cbeb8d3ab
danieldc98 <ddecastro@macbook-air.closewide.close.cathedral.org>
2015-05-11 10:09:02 -0400

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Daniel de Castro on 2015-05-01, 09:57.
+ */
+
+import java.util.*;
+public class Player {
+
+
+    public Player(String pt) {
+        playerType = pt;
+    }
+}
---
+++ src/Tester.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Daniel de Castro on 2015-05-01, 10:02.
+ */
+public class Tester {
+    public static void main(String[] args) {
+    }
+}
